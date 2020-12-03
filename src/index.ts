40d0d5446cc61f30cd19a5dd38ae44cd9306522f
ZeffK <54438151+ZeffK@users.noreply.github.com>
2020-12-03 20:10:03 +0530

    Deployment-changes

--- src/index.ts
+++ src/index.ts
@@ -5,43 +5,51 @@ import { buildSchema } from 'type-graphql';
 import { ApolloServer } from 'apollo-server-express';
 import { User } from './entities/User';
 import redis from 'redis';
-import session from 'express-session';
-import connectRedis from 'connect-redis';
-import { __prod__ } from './constants';
+// import session from 'express-session';
+// import connectRedis from 'connect-redis';
 import { UserResolver } from './resolvers/user';
 import { HelloResolver } from './resolvers/hello';
+import { DATABASE_URL, __prod__ } from './constants';
 const main = async () => {
     await createConnection({
         type: 'postgres',
-        database: 'deardiary',
-        username: 'pruthvi',
+        database: 'karobaar',
+        url: DATABASE_URL,
         logging: true,
         synchronize: true,
         entities: [User],
+        ssl: __prod__ ? true : false,
+        extra: __prod__
+            ? {
+                  ssl: {
+                      rejectUnauthorized: false,
+                  },
+              }
+            : '',
     });
     const app = express();
 
-    const RedisStore = connectRedis(session);
-    const redisClient = redis.createClient();
+    // const RedisStore = connectRedis(session);
+    // const redisClient = redis.createClient();
 
-    app.use(
-        session({
-            name: 'qid',
-            store: new RedisStore({
-                client: redisClient,
-                disableTouch: true,
-            }),
-            cookie: {
-                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years for dev
-                httpOnly: true,
-                secure: __prod__, // cookie works in https?
-                sameSite: 'lax', // csrf settings
-            },
-            saveUninitialized: false,
-            secret: 'verysecret', // HIDE IN PROD!!
-            resave: false,
-        }),
-    );
+    // app.use(
+    //     session({
+    //         name: 'qid',
+    //         store: new RedisStore({
+    //             client: redisClient,
+    //             disableTouch: true,
+    //         }),
+    //         cookie: {
+    //             maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years for dev
+    //             httpOnly: true,
+    //             secure: __prod__, // cookie works in https?
+    //             sameSite: 'lax', // csrf settings
+    //         },
+    //         saveUninitialized: false,
+    //         secret: 'verysecret', // HIDE IN PROD!!
+    //         resave: false,
+    //     }),
+    // );
     const apoloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [HelloResolver, UserResolver],
